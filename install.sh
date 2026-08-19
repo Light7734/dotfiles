a5e514493eed17b0170c8d58a2657dcc26bb1135
light7734 <light7734@tuta.io>
2026-08-19 16:03:19 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ REPO="$(git rev-parse --show-toplevel)"
 CONF="$HOME/.config/"
 
 if [ -d "$REPO/backup" ]; then
-  echo "$REPO/backup already exists, please handle what happened in previous runs before re-attempting and deleting the backup."
+    echo "$REPO/backup already exists, please handle what happened in previous runs before re-attempting and deleting the backup."
 fi
 
 mkdir .backup/
@@ -13,13 +13,19 @@ echo 'Creating backup...'
 cp -rv "$CONF/btop" "$REPO/backup/btop"
 cp -rv "$CONF/nvim" "$REPO/backup/nvim"
 cp -rv "$CONF/kitty" "$REPO/backup/kitty"
+cp -rv "${HOME}/.gitconfig" "$REPO/backup/gitconfig"
+cp -v "${HOME}/.zshrc" "$REPO/backup/zshrc.sh"
 
 echo 'Removing currently installed config files...'
 rm -rv "$CONF/btop"
 rm -rv "$CONF/nvim"
 rm -rv "$CONF/kitty"
+rm -v "$HOME/.zshrc"
+rm -v "$HOME/.gitconfig"
 
 echo 'Installing the new config...'
-cp -rv "$REPO/btop" "$CONF/btop" 
-cp -rv "$REPO/nvim" "$CONF/nvim" 
-cp -rv "$REPO/kitty" "$CONF/kitty" 
+cp -rv "$REPO/btop" "$CONF/btop"
+cp -rv "$REPO/nvim" "$CONF/nvim"
+cp -rv "$REPO/kitty" "$CONF/kitty"
+cp -v "$REPO/zshrc.sh" "$HOME/.zshrc"
+cp -v "$REPO/gitconfig" "$HOME/.gitconfig"
